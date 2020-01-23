62ac44d03a67b488bde90d076e199f4d167f4a21
nagamachan <59175719+nagamachan@users.noreply.github.com>
2020-01-23 19:38:52 +0900

    init

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
@@ -9,6 +10,26 @@ class PostsController < ApplicationController
 
   def create
     Post.create(post_params)
+    redirect_to root_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
     redirect_to root_path
   end
 
